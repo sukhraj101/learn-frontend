cb7aad881ac47cfcd3de7c89d1730bc3f39c0e25
sukhrajsingh101 <sukhrajsingh101@gmail.com>
2025-10-06 18:12:29 +0530

    refactored responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,18 @@ function HomePage () {
         <>
             <section className="heroSection">
                 <Container className="custom" sx={{ marginTop: "auto", marginBottom: "3rem", paddingTop: "1rem" }}>
-                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                        <Grid size={7.5}>
-                            <Typography variant="h2" gutterBottom>
-                            <Typography variant="span" gutterBottom>Hi,</Typography> Lorem ipsum dolor sit amet consectetur.</Typography>
+                    <Grid container rowSpacing={{ xs: 3, sm: 4, md: 6, lg: 6 }} columnSpacing={{ xs: 3, sm: 3, md: 3 }}>
+                        <Grid size={{ xs: 12, md: 6 }}>
+                            <Typography variant="h2" gutterBottom sx={{
+                                fontSize: {
+                                xs: '1.8rem',  // mobile
+                                sm: '1.8rem',  // tablet
+                                md: '2.5rem',  // desktop
+                                lg: '3.5rem',  // desktop large
+                                },
+                            }}>
+                                <Typography variant="span" gutterBottom>Hi,</Typography> Lorem ipsum dolor sit amet consectetur.
+                            </Typography>
                             <Typography variant="body" component="p" gutterBottom sx={{ mb: 0.5, fontWeight: 600  }}>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur dolor praesentium expedita maiores voluptas sed eum hic magnam doloremque sint, tempore.
                             </Typography>
@@ -24,7 +32,7 @@ function HomePage () {
                                 <Button variant="contained" color="primary" size="large">Start LevelUp Course</Button>
                             </Box>
                         </Grid>
-                        <Grid size={4.5}>
+                        <Grid size={{ xs: 12, md: 6 }}>
                             <Box className="skill-cards">
                                 <Skillcard />
                             </Box>
